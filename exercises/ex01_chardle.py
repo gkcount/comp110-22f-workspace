a61a6ab8b07d8dac3832ffb87c5bf731ac6a00cb
Grace Countie <gkcount@ad.unc.edu>
2022-08-29 21:22:51 -0400

    changes made to variable

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -13,25 +13,25 @@ if len(letter) > 1 or len(letter) < 1:
     exit()
 print("Searching for " + letter + " in " + word)
 
-count = int = 0
+number = int = 0
 if word[0] == letter:
     print(letter + " found at index 0")
-    count = count + 1
+    number = number + 1
 if word[1] == letter:
     print(letter + " found at index 1")
-    count = count + 1
+    number = number + 1
 if word[2] == letter:
     print(letter + " found at index 2")
-    count = count + 1
+    number = number + 1
 if word[3] == letter:
     print(letter + " found at index 3")
-    count = count + 1
+    number = number + 1
 if word[4] == letter:
     print(letter + " found at index 4")
-    count = count + 1
-if count == 0:
+    number = number + 1
+if number == 0:
     print("No instances of " + letter + " found in " + word)
-if count == 1:
-    print(str(count) + " intance of " + letter + " found in " + word)
-if count > 1:
-    print(str(count) + " intances of " + letter + " found in " + word)
+if number == 1:
+    print(str(number) + " intance of " + letter + " found in " + word)
+if number > 1:
+    print(str(number) + " intances of " + letter + " found in " + word)
